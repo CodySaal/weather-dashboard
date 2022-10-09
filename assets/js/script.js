f2e2973601d6663fa536303ed709a47c3b636403
Cody <110630101+CodySaal@users.noreply.github.com>
2022-10-08 23:58:52 -0500

    Working Current Weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ var futureForcastEl = document.getElementById("futureWeather")
 
 var key = "f6b1dc9de10dbbcfdf4c08cf6f933425";
 var pastSearches = [];
+var cityLat 
+var cityLon
 
 // Functions
 
@@ -17,11 +19,29 @@ function convertName(cityName){
         .then(function(response){
             return response.json();
         })
-        .then(function(data){
-            console.log(data);
+        .then(function(coordinates){
+            console.log(coordinates);
+            console.log(coordinates[0].lat)
+            console.log(coordinates[0].lon)
+            cityLat = coordinates[0].lat;
+            cityLon = coordinates[0].lon
+            console.log(cityLat)
+            console.log(cityLon)
+            getCurrentWeather(cityLat, cityLon)
+        })
+        
+}
+function getCurrentWeather(cityLat, cityLon){
+    // get one day forecast
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + key)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(currentWeather){
+            console.log(currentWeather)
         })
 }
-
+console.log(cityLat)
 
 // Main function
 function handleSearch(event){
