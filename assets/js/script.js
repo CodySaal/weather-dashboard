a0b49127e8d9507cff7128bb1e4589957d902323
Cody <110630101+CodySaal@users.noreply.github.com>
2022-10-08 23:08:04 -0500

    Working geocoding API

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,27 @@ var futureForcastEl = document.getElementById("futureWeather")
 var key = "f6b1dc9de10dbbcfdf4c08cf6f933425";
 var pastSearches = [];
 
+// Functions
+
+function convertName(cityName){
+    // convert city name to lat and lon
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + ",US&limit=1&appid=" + key)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
+}
+
+
 // Main function
 function handleSearch(event){
     event.preventDefault();
     // get name of city
     var cityName = searchInput.value
     // geoAPI function
+    convertName(cityName);
     // current weather api function
     // 5 day forecast function
 }
